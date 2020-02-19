ed11ca8df7098647ddc4e25868ca8378a0d56bda
Alan Diaz-Montiel <adiazmon@tcd.ie>
2020-02-19 12:09:12 +0000

    unit-testing installation of switch rules

--- tests/unit_testing/roadm/test_install_switch_rule.py
+++ tests/unit_testing/roadm/test_install_switch_rule.py
@@ -2,30 +2,60 @@ import topo.ofc_demo as OFCDemo
 from tests.ofc.failure_recovery.switching_tuples import SwitchTuples as st
 import numpy as np
 
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import figure
-import matplotlib.font_manager
 
-
-def db_to_abs(db_value):
+def install_switch_rules_roadm1_after_transmit(r1):
     """
-    :param db_value: list or float
-    :return: Convert dB to absolute value
+    Expected outcome: ROADM1 will trigger a switch function after the
+    installation of rules. ROADMs 2 & 3 should complain about not
+    being able to find a rule.
+    :return:
     """
-    absolute_value = 10**(db_value/float(10))
-    return absolute_value
+    proc = st()
+    proc.switch_main()
+    for pr in proc.roadm1:
+        r1.install_switch_rule(**pr)
 
 
-def abs_to_db(absolute_value):
+def install_switch_rules_roadm2_after_transmit(r2):
     """
-    :param absolute_value: list or float
-    :return: Convert absolute value to dB
+    Expected outcome: ROADM1 will complain of not being able to
+    find switching rules for any of the channels. The installation
+    of rules into ROADM2 should not trigger the switching function.
+    :return:
     """
-    db_value = 10*np.log10(absolute_value)
-    return db_value
+    proc = st()
+    proc.switch_main()
+    for pr in proc.roadm2:
+        r2.install_switch_rule(**pr)
 
 
-def transmit(net, lt, roadm):
+def install_switch_rules_after_transmit(r1, r2, r3):
+    """
+    Expected outcome: All transmissions will be successful
+    :return:
+    """
+    proc = st()
+    proc.switch_main()
+    for pr in proc.roadm1:
+        r1.install_switch_rule(**pr)
+    for pr in proc.roadm2:
+        r2.install_switch_rule(**pr)
+    for pr in proc.roadm3:
+        r3.install_switch_rule(**pr)
+
+
+if __name__ == '__main__':
+    """
+    Expected outome: only ROADM1 complains about being unable to
+    find switch rules. The other ROADMs shouldn't even know
+    there is a transmission taking place in the network.
+    :return:
+    """
+    net = OFCDemo.build()
+
+    n = net.name_to_node
+    lt1, lt2, lt3 = [n['lt%d' % i] for i in (1, 2, 3)]
+    roadm1, roadm2, roadm3 = [n['roadm%d' % i] for i in (1, 2, 3)]
 
     # Install switch rules into the ROADM nodes
     channels1 = list(np.arange(1, 11, 1))
@@ -35,26 +65,10 @@ def transmit(net, lt, roadm):
     rw = channels1 + channels2 + channels3
 
     # Set resources to use and initiate transmission
-    resources = {'transceiver': lt.name_to_transceivers['t1'],
+    resources = {'transceiver': lt1.name_to_transceivers['t1'],
                  'required_wavelengths': rw}
-    net.transmit(lt, roadm, resources=resources)
+    net.transmit(lt1, roadm1, resources=resources)
 
-
-# if __name__ == '__main__':
-# build topology
-network = OFCDemo.build()
-
-n = network.name_to_node
-lt1, lt2, lt3 = [n['lt%d' % i] for i in (1, 2, 3)]
-roadm1, roadm2, roadm3 = [n['roadm%d' % i] for i in (1, 2, 3)]
-### Run test(s)
-transmit(network, lt1, roadm1)
-
-proc = st()
-proc.switch_main()
-for pr in proc.roadm1:
-    roadm1.install_switch_rule(**pr)
-for pr in proc.roadm3:
-    roadm3.install_switch_rule(**pr)
-
-
+    # install_switch_rules_roadm1_after_transmit(roadm1)
+    install_switch_rules_roadm2_after_transmit(roadm2)
+    # install_switch_rules_after_transmit(roadm1, roadm2, roadm3)
