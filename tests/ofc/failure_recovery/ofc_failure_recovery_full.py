0cf45dd8856f3ce388e3a113ac3d7ee25ce0c577
Alan Diaz <adiazmon@tcd.ie>
2020-03-04 12:46:05 -0800

    edits for plotting

--- tests/ofc/failure_recovery/ofc_failure_recovery_full.py
+++ tests/ofc/failure_recovery/ofc_failure_recovery_full.py
@@ -1,6 +1,6 @@
 import topo.ofc_demo as OFCDemo
 from tests.ofc.switching_tuples import SwitchTuples as st
-from tests.ofc.failure_recovery.plot_osnr import plot_list_osnr
+from tests.ofc.failure_recovery.plot_osnr import plot_list_osnr, plot_1
 import numpy as np
 
 
@@ -61,7 +61,7 @@ net.transmit(lt1, roadm1, resources=resources)
 rw2 = channels3 + channels4 + channels5
 resources2 = {'transceiver': lt6.name_to_transceivers['t1'],
               'required_wavelengths': rw2}
-# net.transmit(lt6, roadm6, resources=resources2)
+net.transmit(lt6, roadm6, resources=resources2)
 
 
 # Monitoring nodes at the receiver-ends
@@ -71,19 +71,20 @@ osnr_r2_r42 = r2_r4_mon.get_list_gosnr()
 r4_r6_mon = n['mon%d' % 52]
 osnr_r4_r6 = r4_r6_mon.get_list_gosnr()
 
-plot_list_osnr([osnr_r2_r42, osnr_r4_r6])
+# plot_1(osnr_r2_r42)
 
 # Adjust NF of amplifier in link between R1 and R2, so that
 # multiple links break.
 # net.mock_nf_adjust('amp3', (30, 90), 'roadm1', 'roadm2')
-# net.mock_amp_gain_adjust('amp3', -10, 'roadm1', 'roadm2')
-#
-# r2_r4_mon = n['mon%d' % 28]
-# osnr_r2_r43 = r2_r4_mon.get_list_osnr()
+net.mock_amp_gain_adjust('amp3', -10, 'roadm1', 'roadm2')
+
+r2_r4_mon = n['mon%d' % 28]
+osnr_r2_r43 = r2_r4_mon.get_list_osnr()
 #
 # r4_r6_mon = n['mon%d' % 52]
 # osnr_r4_r62 = r4_r6_mon.get_list_osnr()
 #
+plot_1(osnr_r2_r43)
 # plot_list_osnr([osnr_r4_r6, osnr_r4_r62])
 #
 # # control procedures for recovery
--- tests/ofc/failure_recovery/plot_osnr.py
+++ tests/ofc/failure_recovery/plot_osnr.py
@@ -1,4 +1,13 @@
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+import matplotlib.font_manager
+
+
+figure(num=None, figsize=(7, 6), dpi=256)
+del matplotlib.font_manager.weight_dict['roman']
+matplotlib.font_manager._rebuild()
+plt.rcParams["font.family"] = "Times New Roman"
+plt.rcParams["font.size"] = 20
 
 
 def plot_osnr(_list):
@@ -17,4 +26,25 @@ def plot_list_osnr(_list):
         plt.plot(element, linestyle='None', linewidth=4, color='g', marker='D', markersize=8, markerfacecolor='None', )
         fig_count += 1
     plt.show()
+
+
+def plot_1(_list):
+    wv = ['1529.6', '1533.2', '1537.2', '1541.2']
+    x = []
+    for j in range(3):
+        x.append(wv.pop())
+        for i in range(9):
+            if j == 2 and i == 8:
+                x.append(wv.pop())
+            else:
+                x.append('')
+
+    plt.plot(_list, linestyle='None', linewidth=4, color='g', marker='D', markersize=10, markerfacecolor='None', )
+    plt.xticks(range(30), x)
+    plt.xlabel("Wavelength index [nm]")
+    plt.ylabel("gOSNR (dB)")
+    plt.savefig('fig2.png', format='png')
+    # plt.show()
+
+    
 
