8543db2f277849d5a6482e138a5992ef63d54776
Alan Diaz <adiazmon@tcd.ie>
2020-04-03 15:47:48 -0400

    modifications for no-monitoring simulations

--- test_exec.py
+++ test_exec.py
@@ -6,13 +6,13 @@
 """
 
 # import tests.monitoring_4_hop_transmission
-# import tests.monitoring_multiple_hop_transmission
+import tests.monitoring_multiple_hop_transmission
 # import tests.analysis_multi_opm
 # import tests.analysis_multihop_multi_opm
 # import tests.monitoring_power_noise_roadm
 # import tests.monitoring_nli_noise
 # import tests.update_rule_roadm
-import tests.monitoring_gosnr_vs_power
+# import tests.monitoring_gosnr_vs_power
 # import tests.monitoring_gosnr_vs_distance_power
 # import tests.analysis_multihop_transmission
 # import tests.gain_power_excursions_verification
