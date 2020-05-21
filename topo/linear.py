9b86d606a6b261a901f7e98d5cf590ced8a31576
Alan Diaz <adiazmon@tcd.ie>
2020-05-21 09:43:10 -0400

    minor edits for PTL paper

--- topo/linear.py
+++ topo/linear.py
@@ -45,7 +45,7 @@ class LinearTopology:
         opm_lab = 'opm'
 
         opm_i = 0
-        span_no = 3
+        span_no = 6
         tmp_qot_id = 1
         for i in range(non-1):
             # Iterate through the number of nodes linearly connected
@@ -72,10 +72,10 @@ class LinearTopology:
             for sp in range(span_no):
                 # Iterate through number of spans in link
                 # span object
-                span = Span('SMF', 80)
+                span = Span('SMF', 100)
                 in_l = amp_lab + str(in_apm_no+1) + us + 'l' + us + roadm_lab + str(r1) + us + roadm_lab + str(r2)
 
-                in_line_amp = net.add_amplifier(in_l, 'EDFA', target_gain=17.6, tmp_qot_id=tmp_qot_id)
+                in_line_amp = net.add_amplifier(in_l, 'EDFA', target_gain=22.0, tmp_qot_id=tmp_qot_id)
                 # adding span and in-line amplifier to link
                 link_r1_r2.add_span(span, in_line_amp)
                 opm_l = opm_lab + us + str(opm_no + 1)  # label OPM
