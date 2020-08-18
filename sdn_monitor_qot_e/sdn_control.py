fb46742ffe68edae1bb5df18eb91868824c476f3
Alan Diaz <adiazmon@tcd.ie>
2020-08-18 13:06:24 -0400

    debugging random allocation

--- sdn_monitor_qot_e/sdn_control.py
+++ sdn_monitor_qot_e/sdn_control.py
@@ -190,7 +190,8 @@ def install_paths(channel_no, signal_ids=None):
     else:
         channels = list(np.arange(1, channel_no + 1))
     # Configure roadms
-    line1, line2 = 82, 83
+    # line1, line2 = 82, 83
+    line1, line2 = 10, 11
 
     # r1: add/drop channels r1<->r5
     for local_port, ch in enumerate(channels, start=1):
@@ -241,7 +242,8 @@ def configure_terminals(channel_no, signal_ids=None):
         channels = list(np.arange(1, channel_no + 1))
     # Port numbering
     eth_ports = list(np.arange(1, channel_no + 2))
-    wdm_ports = list(np.arange(82, 82 + channel_no))
+    # wdm_ports = list(np.arange(82, 82 + channel_no))
+    wdm_ports = list(np.arange(10, 10 + channel_no))
 
     # Configure transceivers
     for tx_id, ch in enumerate(channels):
