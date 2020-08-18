da5b7646302e55d51658772093303507e0847f40
Alan Diaz <adiazmon@tcd.ie>
2020-08-18 17:06:05 -0400

    backtracking sequential case

--- sdn_monitor_qot_e/sdn_control.py
+++ sdn_monitor_qot_e/sdn_control.py
@@ -75,25 +75,28 @@ def run(net):
                 for i in range(count)
                 for node in (net.switches[i], net.terminals[i], net.roadms[i])}
 
+    configure_routers(net.switches)
+
     test_num = 150
     _loads = [9, 27, 81]
     for load in _loads:
         print("Running test for load ", load)
+        install_paths(load)
         test_run = 0
         while test_run < test_num:
             print("Running test no. ", test_run)
-            w_i = loadings[load][test_run]
+            # w_i = loadings[load][test_run]
             # Compute QoT estimation
-            estimation_module(load, str(load), str(test_run), signal_ids=w_i)
+            estimation_module(load, str(load), str(test_run))
 
-            configure_routers(net.switches)
+            # configure_routers(net.switches)
             # Install switching rules to roadms
-            install_paths(load, signal_ids=w_i)
+            # install_paths(load, signal_ids=w_i)
             # assign ripple functions to EDFAs
             configure_amps(net, 15, test_run)
             # configure terminals with port connections
-            term_out_ports = configure_terminals(load, signal_ids=w_i)
-            sleep(2)
+            term_out_ports = configure_terminals(load)
+            # sleep(2)
             # launch transmission at terminals
             transmit(term_out_ports)
             # monitor all channels and write log
@@ -187,10 +190,10 @@ def install_paths(channel_no, signal_ids=None):
     """
     Create switching table for the roadms
     """
-    # if signal_ids:
-    channels = signal_ids
-    # else:
-    #     channels = list(np.arange(1, channel_no + 1))
+    if signal_ids:
+        channels = signal_ids
+    else:
+        channels = list(np.arange(1, channel_no + 1))
     # Configure roadms
     line1, line2 = 82, 83
 
