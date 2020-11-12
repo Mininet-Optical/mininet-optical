81315174bf484c38bf58c9820a1ae0efcefde038
Alan Diaz <adiazmon@tcd.ie>
2020-11-12 10:47:10 -0500

    fixes #26

--- node.py
+++ node.py
@@ -281,13 +281,13 @@ class LineTerminal(Node):
             signalInfoDict[optical_signal]['gosnr'] = gosnr
             if gosnr < 20:
                 print("*** %s - %s.receiver.%s: Failure!\ngOSNR: %f dB" %
-                      (optical_signal, self.__class__.__name__, self.name, abs_to_db(gosnr)))
+                      (optical_signal, self.__class__.__name__, self.name, gosnr))
                 signalInfoDict[optical_signal]['success'] = False
                 self.signal_info_dict_transceiver[in_port] = signalInfoDict
                 self.receiver_callback(in_port, signalInfoDict)
             else:
                 print("*** %s - %s.receiver.%s: Success!\ngOSNR: %f dB" %
-                      (optical_signal, self.__class__.__name__, self.name, abs_to_db(gosnr)))
+                      (optical_signal, self.__class__.__name__, self.name, gosnr))
                 signalInfoDict[optical_signal]['success'] = True
                 self.signal_info_dict_transceiver[in_port] = signalInfoDict
                 self.receiver_callback(in_port, signalInfoDict)
@@ -1074,9 +1074,9 @@ class Monitor(Node):
             output_power = optical_signal.loc_in_to_state[self.component]['power']
             ase_noise = optical_signal.loc_in_to_state[self.component]['ase_noise']
             nli_noise = optical_signal.loc_in_to_state[self.component]['nli_noise']
-        osnr_linear = output_power / (ase_noise + nli_noise * (12.5e9 / 32.0e9))
-        osnr = abs_to_db(osnr_linear)
-        return osnr
+        gosnr_linear = output_power / (ase_noise + nli_noise * (12.5e9 / 32.0e9))
+        gosnr = abs_to_db(gosnr_linear)
+        return gosnr
 
     def __repr__(self):
         return "<name: %s, component: %s,>" % (self.name, self.component)
