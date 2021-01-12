c70c229ee609e00dd2e335f1e26ad860e8ef1b46
jiakaiyu <jiakaiyu@email.arizona.edu>
2021-01-12 02:45:54 -0700

    Update net_simu.py

--- ofcdemo/net_simu.py
+++ ofcdemo/net_simu.py
@@ -12,8 +12,16 @@ from units import *
 
 # Units
 
+
 km = dB = dBm = 1.0
 m = .001
+
+# Parameters
+
+NUM_WAV = 20
+# ROADM port numbers (input and output)
+LINE_PORT = NUM_WAV
+LINE_PORT2 = NUM_WAV+1
 
 
 # Physical model API helpers
@@ -54,27 +62,18 @@ def twoRoadmPhyNetwork( lengths=[50*km] ):
     net = Network()
 
     # Network nodes
-    transceivers = [ ( 't1', 0*dBm, 'C' ), ( 't2', 0*dBm, 'C' ) ]
+    transceivers = [ ( 't%d' % i, 0*dBm, 'C' )  for i in range(1, NUM_WAV+1) ]
     # two terminals, each includes t1 and t2
     terminals = tx1, tx2 = [
         net.add_lt( name, transceivers=transceivers, monitor_mode=mode )
         for name,mode in [('tx1','in'),('tx2','in')] ]
-    print('===tl===', terminals, '\n',
+    """print('===tl===', terminals, '\n',
             tx1.transceivers[0].name, tx1.transceivers[0].operation_power,tx1.transceivers[0].id, '\n',
             tx1.transceivers[1].name, tx1.transceivers[1].operation_power,tx1.transceivers[1].id, '\n',
             tx2.transceivers[0].name, tx2.transceivers[0].operation_power,tx2.transceivers[0].id, '\n',
             tx2.transceivers[1].name, tx2.transceivers[1].operation_power,tx2.transceivers[1].id)
+    #"""
     roadms = r1, r2 = [ net.add_roadm( 'r%d' % i ) for i in (1, 2) ]
-
-    # ROADM port numbers (input and output)
-    LINE_PORT = 2
-
-    ADD_DROP_0 = 0
-    ADD_DROP_1 = 1
-
-    # Line terminal port numbers (input and output)
-    TX_0 = 0
-    TX_1 = 1
 
     # Convenience alias
     link = net.add_link
@@ -106,7 +105,13 @@ def twoRoadmPhyNetwork( lengths=[50*km] ):
           spans=spans )
 
     # Local add/drop links between terminals/transceivers and ROADMs
-    # tx1:t1->r1
+    for add_drop_port in range(NUM_WAV):
+        link( tx1, r1, src_out_port=add_drop_port, dst_in_port=add_drop_port, spans=[Span(1*m)] )
+        link( r1, tx1, src_out_port=add_drop_port, dst_in_port=add_drop_port, spans=[Span(1*m)] )
+        link( tx2, r2, src_out_port=add_drop_port, dst_in_port=add_drop_port, spans=[Span(1*m)] )
+        link( r2, tx2, src_out_port=add_drop_port, dst_in_port=add_drop_port, spans=[Span(1*m)] )
+
+    """# tx1:t1->r1
     link( tx1, r1, src_out_port=TX_0, dst_in_port=ADD_DROP_0, spans=[Span(1*m)] )
     # tx1:t2->r1
     link( tx1, r1, src_out_port=TX_1, dst_in_port=ADD_DROP_1, spans=[Span(1*m)] )
@@ -123,21 +128,62 @@ def twoRoadmPhyNetwork( lengths=[50*km] ):
     link( r2, tx2, src_out_port=ADD_DROP_0, dst_in_port=TX_0, spans=[Span(1*m)] )
     # tx2:t2<-r2
     link( r2, tx2, src_out_port=ADD_DROP_1, dst_in_port=TX_1, spans=[Span(1*m)] )
+    #"""
+
+    return net
+
+
+# Physical model test
+
+def twoRoadmPhyTest():
+    # ROADM port numbers (input and output)
+    LINE_PORT = NUM_WAV
+    LINE_PORT2 = NUM_WAV+1
+
+    #ADD_DROP_0 = 0
+    #ADD_DROP_1 = 1
+
+    # Line terminal port numbers (input and output)
+    #TX_0 = 0
+    #TX_1 = 1
+
+    "Create a single link and monitor its OSNR and gOSNR"
+    net = twoRoadmPhyNetwork( lengths=[50*km, 50*km, 50*km] )
+    nodes = net.name_to_node
+    tx1, tx2 = nodes[ 'tx1' ], nodes[ 'tx2' ]
+    roadms = r1, r2 = nodes[ 'r1' ], nodes[ 'r2' ]
+    t11, t12 = tx1.name_to_transceivers['t1'],tx1.name_to_transceivers['t2']
+    t21, t22 = tx2.name_to_transceivers['t1'],tx2.name_to_transceivers['t2']
 
     # Configure ROADMS to add/drop channel 1 to local terminal
-    r1.install_switch_rule(
-        rule_id = 1, in_port=ADD_DROP_0, out_port=LINE_PORT, signal_indices=[1] )
-    r1.configure_voa(channel_id = 1, output_port = LINE_PORT,operational_power_dB = 0)
-    r1.install_switch_rule(
-        rule_id = 2, in_port=ADD_DROP_1, out_port=LINE_PORT, signal_indices=[2])
-    r1.configure_voa(2, LINE_PORT, 2)
+    chs = ch1, ch2 = 1, 2
+    route = [('tx1', 'r1', 'r2', 'tx2'), chs]
+    path = route[0]
+    rule_id_dict = {'r1':1, 'r2':1}
 
+    for i in range(1, len(path) - 1 ):
+        node1, roadm, node2 = path[i-1], path[i], path[i+1]
+        if i == 1:
+            for ch in chs:
+                nodes[roadm].install_switch_rule(rule_id = rule_id_dict[roadm], in_port=ch-1, out_port=LINE_PORT, signal_indices=[ch] )
+                rule_id_dict[roadm] += 1
+        elif i == len(path) - 2:
+            for ch in chs:
+                nodes[roadm].install_switch_rule(rule_id = rule_id_dict[roadm], in_port=LINE_PORT, out_port=ch-1, signal_indices=[ch] )
+                rule_id_dict[roadm] += 1
+        else:
+            nodes[roadm].install_switch_rule(rule_id = rule_id_dict[roadm], in_port=LINE_PORT, out_port=LINE_PORT2, signal_indices=[ch] )
+            rule_id_dict[roadm] += 1
+
+    """r1.install_switch_rule(
+        rule_id = 1, in_port=ch1-1, out_port=LINE_PORT, signal_indices=[ch1] )
+    r1.install_switch_rule(
+        rule_id = 2, in_port=ch2-1, out_port=LINE_PORT, signal_indices=[ch2])
     r2.install_switch_rule(
-        rule_id = 1, in_port=LINE_PORT, out_port=TX_0, signal_indices=[1])
-    r2.configure_voa(1, TX_0, 0)
+        rule_id = 1, in_port=LINE_PORT, out_port=ch1-1, signal_indices=[ch1])
     r2.install_switch_rule(
-        rule_id = 2, in_port=LINE_PORT, out_port=TX_1, signal_indices=[2])
-    r2.configure_voa(2, TX_1, 2)
+        rule_id = 2, in_port=LINE_PORT, out_port=ch2-1, signal_indices=[ch2])
+    #"""
 
     print( '*** ROADM connections and flow table' )
     for node in r1, r2:
@@ -148,28 +194,6 @@ def twoRoadmPhyNetwork( lengths=[50*km] ):
     print( '*** Checking correct ROADM flow tables' )
     print(r1, r1.switch_table)
     print(r2, r2.switch_table)
-    return net
-
-
-# Physical model test
-
-def twoRoadmPhyTest():
-    # ROADM port numbers (input and output)
-    LINE_PORT = 2
-
-    ADD_DROP_0 = 0
-    ADD_DROP_1 = 1
-
-    # Line terminal port numbers (input and output)
-    TX_0 = 0
-    TX_1 = 1
-    "Create a single link and monitor its OSNR and gOSNR"
-    net = twoRoadmPhyNetwork( lengths=[50*km, 50*km, 50*km] )
-    nodes = net.name_to_node
-    tx1, tx2 = nodes[ 'tx1' ], nodes[ 'tx2' ]
-    r1, r2 = nodes[ 'r1' ], nodes[ 'r2' ]
-    t11, t12 = tx1.name_to_transceivers['t1'],tx1.name_to_transceivers['t2']
-    t21, t22 = tx2.name_to_transceivers['t1'],tx2.name_to_transceivers['t2']
 
     # start test
     # launch power 0dbm
@@ -177,10 +201,10 @@ def twoRoadmPhyTest():
     t12.operation_power = db_to_abs(2)
     t21.operation_power = db_to_abs(2)
     t22.operation_power = db_to_abs(2)
-    r1.configure_voa(channel_id = 1, output_port = LINE_PORT,operational_power_dB = 2)
-    r1.configure_voa(channel_id =2, output_port = LINE_PORT, operational_power_dB = 2)
-    r2.configure_voa(channel_id =1, output_port = TX_0, operational_power_dB = 2)
-    r2.configure_voa(channel_id =2, output_port = TX_1, operational_power_dB = 2)
+    r1.configure_voa(channel_id = ch1, output_port = LINE_PORT,operational_power_dB = 2)
+    r1.configure_voa(channel_id =ch2, output_port = LINE_PORT, operational_power_dB = 2)
+    r2.configure_voa(channel_id =ch1, output_port = ch1-1, operational_power_dB = 2)
+    r2.configure_voa(channel_id =ch2, output_port = ch2-1, operational_power_dB = 2)
     print( '*** Starting test transmission...' )
     tx1.configure_terminal(tx1.transceivers[0], 1)
     tx1.configure_terminal(tx1.transceivers[1], 2)
@@ -202,10 +226,10 @@ def twoRoadmPhyTest():
     t12.operation_power = db_to_abs(-5)
     t21.operation_power = db_to_abs(-5)
     t22.operation_power = db_to_abs(-5)
-    r1.configure_voa(channel_id = 1, output_port = LINE_PORT,operational_power_dB = -5)
-    r1.configure_voa(channel_id =2, output_port = LINE_PORT, operational_power_dB = -5)
-    r2.configure_voa(channel_id =1, output_port = TX_0, operational_power_dB = -5)
-    r2.configure_voa(channel_id =2, output_port = TX_1, operational_power_dB = -5)
+    r1.configure_voa(channel_id = ch1, output_port = LINE_PORT,operational_power_dB = -5)
+    r1.configure_voa(channel_id =ch2, output_port = LINE_PORT, operational_power_dB = -5)
+    r2.configure_voa(channel_id =ch1, output_port = ch1-1, operational_power_dB = -5)
+    r2.configure_voa(channel_id =ch2, output_port = ch2-1, operational_power_dB = -5)
     print( '*** Starting test transmission...' )
     tx1.configure_terminal(tx1.transceivers[0], 1)
     tx1.configure_terminal(tx1.transceivers[1], 2)
