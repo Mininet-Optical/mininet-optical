12a28c38379e76053a2d8d5ae0e084a5868a4df6
lantz <rlantz@cs.stanford.edu>
2021-03-20 14:41:43 -0700

    Update RunTests.sh (#39)
    - run more tests
    - accumulate and report results
    
    Note most of the tests are failing now.

--- tests/RunTests.sh
+++ tests/RunTests.sh
@@ -1,13 +1,45 @@
 #!/bin/bash
-#Set Necissary Python paths for execution
 
+# Exit on non-handled error
 set -e
-#export PYTHONPATH=$PWD
-export PYTHONPATH=..
-echo "______update_rule_roadm.py______"
-python update_rule_roadm.py
-echo "______singlelinktests-ports.py______"
-python singlelinktests-ports.py
-echo "______multi_signal_transmissionports.py______"
-python multi_signal_transmission.py
-# echo -e "$?"
+
+# Set necessary python path for execution
+testdir=$(dirname $0)
+export PYTHONPATH=$testdir/..
+
+# Accumulate passed and failed tests
+tests=(*.py)
+passed=()
+failed=()
+
+# Run all scripts in this directory
+for test in ${tests[@]}; do
+    echo -e "**** Running $test\n"
+    if python $test; then
+        echo -e "\n**** PASSED: $test\n"
+        passed+=($test)
+    else
+        echo -e "\n**** FAILED: $test\n"
+        failed+=($test)
+    fi
+done
+
+# Report results
+echo -e "**** Test results:\n"
+
+for test in ${passed[@]}; do
+    echo "**** PASSED: $test"
+done
+echo
+for test in ${failed[@]}; do
+    echo "**** FAILED: $test"
+done
+
+# Succeed if no tests failed
+passcount=${#passed[@]}
+failcount=${#failed[@]}
+echo
+echo "**** $passcount tests passed"
+echo "**** $failcount tests failed"
+echo "**** Exiting with code $failcount."
+exit $failcount
