372aae5cf455825e0adf1262de9ad50df534510f
lantz <rlantz@cs.stanford.edu>
2021-04-06 21:00:56 -0700

    Updates for tutorial #2 (#41)

--- examples/simplelink.py
+++ examples/simplelink.py
@@ -6,11 +6,16 @@ simplelink.py: simple link between two terminals
 This is very close to the simplest fully emulated packet-optical
 network that we can create.
 
-To configure the optical network using the REST API:
+To configure the optical network using the REST API, *open another
+window* and use the following commands at the bash prompt (replacing
+localhost with the IP address of this system as needed):
 
-curl 'localhost:8080/connect?node=t1&ethPort=1&wdmPort=2&channel=1'
-curl 'localhost:8080/turn_on?node=t1'
-curl 'localhost:8080/turn_on?node=t2'
+mn=localhost:8080; t1=$mn; t2=$mn
+
+curl "$t1/connect?node=t1&ethPort=1&wdmPort=2&channel=1"
+curl "$t2/connect?node=t2&ethPort=1&wdmPort=2&channel=1"
+curl "$t1/turn_on?node=t1"
+curl "$t2/turn_on?node=t2"
 """
 
 from dataplane import (Terminal, OpticalLink, OpticalNet as Mininet,
@@ -19,7 +24,7 @@ from rest import RestServer
 from ofcdemo.demolib import OpticalCLI as CLI
 
 from mininet.topo import Topo
-from mininet.log import setLogLevel
+from mininet.log import setLogLevel, info
 from mininet.clean import cleanup
 
 class SimpleLinkTopo(Topo):
@@ -32,13 +37,13 @@ class SimpleLinkTopo(Topo):
         h1, h2 = self.addHost('h1'), self.addHost('h2')
         # Optical network elements
         params = {'transceivers': [('tx1',0*dBm,'C')],
-                  'receiver_threshold': 15.0*dB } # FIXME
+                  'monitor_mode': 'in'}
         t1 = self.addSwitch('t1', cls=Terminal, **params)
         t2 = self.addSwitch('t2', cls=Terminal, **params)
         # Ethernet links
         self.addLink(h1, t1, port2=1)
         self.addLink(h2, t2, port2=1)
-        # WDM links
+        # WDM link
         self.addLink(t1, t2, cls=OpticalLink, port1=2, port2=2,
                      boost=('boost', {'target_gain':17*dB}),
                      spans=[25*km])
@@ -54,6 +59,7 @@ if __name__ == '__main__':
 
     net.start()
     restServer.start()
+    info(__doc__)
     CLI(net)
     restServer.stop()
     net.stop()
