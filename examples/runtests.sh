1c20a663a1cf85260e8043162b7208e48e35d77f
Bob Lantz <rlantz@cs.stanford.edu>
2021-05-10 18:38:56 -0700

    Add missing runtests.sh
    - inadvertently omitted

---
+++ examples/runtests.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Exit on non-handled error
+set -e
+
+# Set necessary python path for execution
+testdir=$(dirname $0)
+timeout=60  # Fail test if it takes over 60s
+run="env PYTHONPATH=$testdir/.. timeout $timeout python3"
+
+# Accumulate passed and failed tests
+tests=($testdir/*.py)
+passed=()
+failed=()
+
+# Run all scripts in this directory
+args='test'
+for test in ${tests[@]}; do
+    echo -e "**** Running $test\n"
+    echo "$run $test $args"
+    if $run $test $args; then
+        echo -e "\n**** PASSED: $test\n"
+        passed+=($test)
+    else
+        echo -e "\n**** FAILED: $test\n"
+        failed+=($test)
+    fi
+done
+
+# Report results
+echo -e "**** Test results:\n"
+
+for test in ${passed[@]}; do
+    echo "**** PASSED: $test"
+done
+echo
+for test in ${failed[@]}; do
+    echo "**** FAILED: $test"
+done
+
+# Succeed if no tests failed
+passcount=${#passed[@]}
+failcount=${#failed[@]}
+echo
+echo "**** $passcount tests passed"
+echo "**** $failcount tests failed"
+echo "**** Exiting with code $failcount."
+exit $failcount
