87e20316d3c0b91c7fcfbe87a10aff23557b16c3
jiakaiyu <jiakaiyu@email.arizona.edu>
2021-05-24 05:04:05 -0700

    Update Control_Test_Lum.py

--- ofcdemo/Control_Test_Lum.py
+++ ofcdemo/Control_Test_Lum.py
@@ -1,31 +1,6 @@
-from network import Network
-from link import Span as Fiber, SpanTuple as Segment
-from Simulation_API import Mininet_Optical_Simu_API
+
 from lumentum_NETCONF_API import Lumentum_NETCONF
-from node import Transceiver
-from units import *
-from collections import defaultdict
-import random
-from collections import defaultdict
-import numpy as np
-from ofcdemo.fakecontroller import (
-    RESTProxy, TerminalProxy, ROADMProxy, OFSwitchProxy,
-    fetchNodes, fetchLinks, fetchPorts, fetchOSNR )
 
-
-from dataplane import (Terminal, ROADM, OpticalLink,
-                       OpticalNet as Mininet, km, m, dB, dBm)
-from rest import RestServer
-from ofcdemo.demolib import OpticalCLI as CLI
-
-from mininet.node import OVSBridge, Host
-from mininet.topo import Topo
-from mininet.log import setLogLevel
-from mininet.clean import cleanup
-
-from os.path import dirname, realpath, join
-from subprocess import run
-from sys import argv
 
 
 km = dB = dBm = 1.0
@@ -80,7 +55,7 @@ LumentumName_to_IP = {}
 LumentumName_to_IP['L1'], LumentumName_to_IP['L2'], \
 LumentumName_to_IP['L3'], LumentumName_to_IP['L4'], \
 LumentumName_to_IP['L5'], LumentumName_to_IP['L6'] \
-    = '10.104.1','10.104.2','10.104.3','10.104.4','10.104.5','10.104.6'
+    = '10.104.1.1','10.104.1.2','10.104.1.3','10.104.1.4','10.104.1.5','10.104.1.6'
 
 
 
@@ -103,39 +78,40 @@ class Lumentum_Control_NETCONF(object):
         # ingress and egress nodes only use MUX and DEMUX respectively. Intermediate nodes use DEMUX -> MUX.
         for i in range(len(path)):
             # intermediate ROADM nodes using 2 Lumentum nodes (4 WSSs)
-            if i != 0 or i != len(path)-1:
+            print(i)
+            if i != 0 and i != len(path)-1:
                 Lumentum_this_out, Lumentum_next = NodeLink_to_LumentumLink[path[i], path[i + 1]]
                 Lumentum_previous, Lumentum_this_in = NodeLink_to_LumentumLink[path[i - 1], path[i]]
-                Lumentum_NETCONF_Agent._ConfigWSS(node_ip=LumentumName_to_IP[Lumentum_this_in], status='add', conn_id=ruleID,
+                Lumentum_NETCONF_Agent._ConfigWSS(node_ip=LumentumName_to_IP[Lumentum_this_in], status='in-service', conn_id=ruleID,
                                                   module_id=1, input_port=4100+THRUPORT, output_port=4201,
                                                   start_freq=start_freq, end_freq=end_freq, attenuation=0,
-                                                  block=False, name='CH' + str(channel))
-                Lumentum_NETCONF_Agent._ConfigWSS(node_ip=LumentumName_to_IP[Lumentum_this_in], status='add', conn_id=ruleID,
+                                                  block='false', name='CH' + str(channel))
+                Lumentum_NETCONF_Agent._ConfigWSS(node_ip=LumentumName_to_IP[Lumentum_this_in], status='in-service', conn_id=ruleID,
                                                   module_id=2, input_port=5101, output_port=5200+THRUPORT,
                                                   start_freq=start_freq, end_freq=end_freq, attenuation=0,
-                                                  block=False, name='CH' + str(channel))
-                Lumentum_NETCONF_Agent._ConfigWSS(node_ip=LumentumName_to_IP[Lumentum_this_out], status='add', conn_id=ruleID,
+                                                  block='false', name='CH' + str(channel))
+                Lumentum_NETCONF_Agent._ConfigWSS(node_ip=LumentumName_to_IP[Lumentum_this_out], status='in-service', conn_id=ruleID,
                                                   module_id=1, input_port=4100 + THRUPORT, output_port=4201,
                                                   start_freq=start_freq, end_freq=end_freq, attenuation=0,
-                                                  block=False, name='CH' + str(channel))
-                Lumentum_NETCONF_Agent._ConfigWSS(node_ip=LumentumName_to_IP[Lumentum_this_out], status='add', conn_id=ruleID,
+                                                  block='false', name='CH' + str(channel))
+                Lumentum_NETCONF_Agent._ConfigWSS(node_ip=LumentumName_to_IP[Lumentum_this_out], status='in-service', conn_id=ruleID,
                                                   module_id=2, input_port=5101, output_port=5200 + THRUPORT,
                                                   start_freq=start_freq, end_freq=end_freq, attenuation=0,
-                                                  block=False, name='CH' + str(channel))
+                                                  block='false', name='CH' + str(channel))
             # Add/drop using Lumentum WSSs
             if i == 0 or i == len(path)-1:
                 if i == 0:
                     Lumentum_this_add_drop, Lumentum_next = NodeLink_to_LumentumLink[path[i], path[i+1]]
                 else:
                     Lumentum_previous, Lumentum_this_add_drop = NodeLink_to_LumentumLink[path[i-1], path[i]]
-                Lumentum_NETCONF_Agent._ConfigWSS(node_ip=LumentumName_to_IP[Lumentum_this_add_drop], status='add', conn_id=ruleID,
+                Lumentum_NETCONF_Agent._ConfigWSS(node_ip=LumentumName_to_IP[Lumentum_this_add_drop], status='in-service', conn_id=ruleID,
                                                   module_id=1, input_port=4100 + ADD_DROP, output_port=4201,
                                                   start_freq=start_freq, end_freq=end_freq, attenuation=0,
-                                                  block=False, name='CH' + str(channel))
-                Lumentum_NETCONF_Agent._ConfigWSS(node_ip=LumentumName_to_IP[Lumentum_this_add_drop], status='add', conn_id=ruleID,
+                                                  block='false', name='CH' + str(channel))
+                Lumentum_NETCONF_Agent._ConfigWSS(node_ip=LumentumName_to_IP[Lumentum_this_add_drop], status='in-service', conn_id=ruleID,
                                                   module_id=2, input_port=5101, output_port=5200 + ADD_DROP,
                                                   start_freq=start_freq, end_freq=end_freq, attenuation=0,
-                                                  block=False, name='CH' + str(channel))
+                                                  block='false', name='CH' + str(channel))
 
 
     def uninstallPath(self, path, ruleID):
@@ -144,7 +120,7 @@ class Lumentum_Control_NETCONF(object):
 
         for i in range(len(path)):
             # del intermediate ROADM nodes using West+East Lumentum nodes (4 WSSs)
-            if i != 0 or i != len(path) - 1:
+            if i != 0 and i != len(path) - 1:
                 Lumentum_this_out, Lumentum_next = NodeLink_to_LumentumLink[path[i], path[i + 1]]
                 Lumentum_previous, Lumentum_this_in = NodeLink_to_LumentumLink[path[i - 1], path[i]]
                 Lumentum_NETCONF_Agent._ConfigWSS(node_ip=LumentumName_to_IP[Lumentum_this_in], status='del', conn_id=ruleID,
@@ -171,8 +147,8 @@ class Lumentum_Control_NETCONF(object):
         Lumentum_previous, Lumentum_Desination = NodeLink_to_LumentumLink[path[-2], path[-1]]
         S_WSS_info = Lumentum_NETCONF_Agent._WSSMonitor(LumentumName_to_IP[Lumentum_Source])
         D_WSS_info = Lumentum_NETCONF_Agent._WSSMonitor(LumentumName_to_IP[Lumentum_Desination])
-        print(S_WSS_info['MUX'][ruleID], S_WSS_info['DEMUX'][ruleID])
-        print(D_WSS_info['MUX'][ruleID], D_WSS_info['DEMUX'][ruleID])
+        print(S_WSS_info)
+        print(D_WSS_info)
 
 
 def Control_Test():
@@ -181,7 +157,7 @@ def Control_Test():
     ####### Lumentum Test ########
     path = ['r4', 'r3', 'r2', 'r1']
     Controller = Lumentum_Control_NETCONF()
-    Controller.installPath(path=path, channel=5, ruleID=1)
+    Controller.installPath(path=path, channel=5, ruleID='1')
     Controller.channel_monitor(path=path, ruleID=1)
     #Controller.uninstallPath(path=path, ruleID=1)
 
