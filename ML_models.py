6bb1299aa9fff7a4eeb3e902f64bb64916284e1b
manny118 <emmanueljones2090@gmail.com>
2022-07-17 16:25:32 +0100

    Code contributions

---
+++ ML_models.py
@@ -0,0 +1,32 @@
+# The TensorFLow library is needed to use ML models. 
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import load_model
+from tensorflow import math as TFmath
+
+
+# Custom loss function
+def customLoss(y_actual, y_pred):
+    loaded_size = tf.dtypes.cast(TFmath.count_nonzero(y_actual), tf.float32)
+    y_pred_cast_unloaded_to_zero = TFmath.divide_no_nan(TFmath.multiply(y_pred, y_actual), y_actual)
+    error = TFmath.abs(TFmath.subtract(y_pred_cast_unloaded_to_zero, y_actual))
+    custom_loss = TFmath.divide(TFmath.reduce_sum(error), loaded_size)
+    return custom_loss
+
+
+# Function for the prediction of WDG
+def predictWDG(input_features, ML_model):
+    if ML_model == 'ML_1':
+        model_name = 'ML_Model_Booster_rd1_co1'
+    elif ML_model == 'ML_2':
+        model_name = 'ML_Model_Preamp_rd2_co1'
+    elif ML_model == 'ML_3':
+        model_name = 'ML_Model_LineAmp1'
+
+    # Load the model and use a custom loss function
+    model = load_model(
+        model_name,
+        custom_objects={"customLoss": customLoss}
+    )
+    return model.predict(input_features)
+
